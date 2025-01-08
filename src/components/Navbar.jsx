70bcbb5940c59a9c5bb2aa52d29290780ec3039c
Romain MURIER <murierromain@gmail.com>
2025-01-08 13:55:13 +0100

    fix: number of items in wishlist

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router";
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const [wishlistCount, setWishlistCount] = useState(0);
+
+  useEffect(() => {
+    // Récupérer la wishlist depuis le localStorage
+    const storedWishlist = localStorage.getItem("wishlist");
+    if (storedWishlist) {
+      setWishlistCount(JSON.parse(storedWishlist).length);
+    }
+  }, []);
+
   return (
     <div className={styles.verticalNavbar}>
       <Link to="/" className={styles.navItem}>
@@ -12,7 +22,7 @@ const Navbar = () => {
         Liste des films
       </Link>
       <Link to="/wishlist" className={styles.navItem}>
-        Wishlist
+        Wishlist ({wishlistCount})
       </Link>
     </div>
   );
